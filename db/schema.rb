b2affa3fceafdd52ab9d7ddce1d949d35ae09e33
albina <albinarubydev@gmail.com>
2026-02-02 20:13:56 +0400

    feat: прикрепление ссылки к вопросу через вложенную форму

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_26_093153) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_01_121949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,15 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_26_093153) do
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
+  create_table "links", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.bigint "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_links_on_question_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -79,6 +88,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_26_093153) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users", column: "author_id"
+  add_foreign_key "links", "questions"
   add_foreign_key "questions", "answers", column: "best_answer_id"
   add_foreign_key "questions", "users", column: "author_id"
 end
