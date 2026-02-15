467f8e80c3abd40391457905eba9b887004fbdb2
albina <albinarubydev@gmail.com>
2026-02-15 21:27:03 +0400

    feat: вывод gist вместо ссылки у ответа

--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -9,8 +9,75 @@ feature 'User can add links to answer', %q(
   given!(:question) { create(:question) }
   given(:gist_url) { 'https://gist.github.com/AlbinaRubyDev/8483bc731953ac17f94b757e6050774b' }
   given(:gist_url_2) { 'https://gist.github.com/AlbinaRubyDev/56d96eb99744e125ae7be0a5270c9ea8' }
+  given(:url_1) { 'https://translate.google.com/' }
+  given(:url_2) { 'https://mail.google.com/' }
 
-  scenario 'User adds link when asks answer', js: true do
+  scenario 'User adds link when writing a answer', js: true do
+    sign_in(user)
+
+    visit question_path(question)
+
+    expect(page).to have_css('turbo-frame#new_answer')
+
+    within 'turbo-frame#new_answer' do
+      fill_in 'Body', with: 'answer text text text'
+
+      click_on 'Add link'
+
+      fill_in 'Link name', with: 'My url'
+      fill_in 'Url', with: url_1
+
+      click_on 'Submit answer'
+    end
+
+    within 'turbo-frame#answers' do
+      expect(page).to have_link 'My url', href: url_1
+    end
+  end
+
+  scenario 'User adds links when writing a answer', js: true do
+    sign_in(user)
+
+    visit question_path(question)
+
+    #временно, для отладки редко выпадающей ошибки "expected to find css 'turbo-frame#new_answer'"
+    expect(page).to have_current_path(question_path(question), ignore_query: true)
+
+    expect(page).to have_css('turbo-frame#new_answer')
+
+    within 'turbo-frame#new_answer' do
+      fill_in 'Body', with: 'answer text text text'
+
+      click_on 'Add link'
+
+      all('[data-link-block="true"]').last.tap do |block|
+        within block do
+          fill_in 'Link name', with: 'My url 1'
+          fill_in 'Url', with: url_1
+        end
+      end
+
+      click_on 'Add link'
+
+      all('[data-link-block="true"]').last.tap do |block|
+        within block do
+          fill_in 'Link name', with: 'My url 2'
+          fill_in 'Url', with: url_2
+        end
+      end
+
+      click_on 'Submit answer'
+    end
+
+    expect(page).to have_css('turbo-frame#answers')
+
+    within 'turbo-frame#answers' do
+      expect(page).to have_link 'My url 1', href: url_1
+      expect(page).to have_link 'My url 2', href: url_2
+    end
+  end
+
+  scenario 'User adds link to a gist when writing a answer', js: true do
     sign_in(user)
 
     visit question_path(question)
@@ -29,11 +96,11 @@ feature 'User can add links to answer', %q(
     end
 
     within 'turbo-frame#answers' do
-      expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_content 'Hello, World!'
     end
   end
 
-  scenario 'User adds links when asks answer', js: true do
+  scenario 'User adds links to a gist when writing a answer', js: true do
     sign_in(user)
 
     visit question_path(question)
@@ -70,8 +137,8 @@ feature 'User can add links to answer', %q(
     expect(page).to have_css('turbo-frame#answers')
 
     within 'turbo-frame#answers' do
-      expect(page).to have_link 'My gist 1', href: gist_url
-      expect(page).to have_link 'My gist 2', href: gist_url_2
+      expect(page).to have_content 'Hello, World!'
+      expect(page).to have_content 'gist for features test'
     end
   end
 end
