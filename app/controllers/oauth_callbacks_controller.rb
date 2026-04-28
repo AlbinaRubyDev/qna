eacdcedd9711c27374c8677a809560c9a8b1a2de
albina <albinarubydev@gmail.com>
2026-04-28 20:30:40 +0400

    refactor: дублирование в контроллере убрано

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -1,32 +1,24 @@
 class OauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-
-    if @user&.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
-    else
-      redirect_to root_path, alert: 'Something went wrong'
-    end
+    authentication_processing('Github')
   end
 
   def facebook
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-
-    if @user&.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    else
-      redirect_to root_path, alert: 'Something went wrong'
-    end
+    authentication_processing('Facebook')
   end
 
   def twitter2
+    authentication_processing('Twitter2')
+  end
+
+  private
+
+  def authentication_processing(provider)
     @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user&.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Twitter2') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
     else
       redirect_to root_path, alert: 'Something went wrong'
     end
